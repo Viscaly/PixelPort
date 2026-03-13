1977db1f6c7f3be22e2453a487d7bb925045cf03
Viscaly <266098764+Viscaly@users.noreply.github.com>
2026-03-13 15:44:29 +0200

    Add files via upload

---
+++ Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PixelPort.Components.Models;
+
+public class LoginModel
+{
+    [Required]
+    public string Username { get; set; } = "";
+
+    [Required]
+    public string Password { get; set; } = "";
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PixelPort.Components.Models
+{
+    [Table("Users")] 
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Username { get; set; } = "";
+
+        public string PasswordHash { get; set; } = "";
+
+        public bool IsAdmin { get; set; }
+    }
+}
